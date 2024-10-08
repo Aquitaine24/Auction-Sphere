80864ce085600969d9f582e465385c282325b523
Azmaan Din <azmaand1n@gmail.com>
2024-10-08 22:11:36 +1300

    Fixed erthem network

--- src/ethereum/auctionFactory.ts
+++ src/ethereum/auctionFactory.ts
@@ -2,7 +2,7 @@ import { Contract, ethers } from "ethers";
 import provider from "./provider";
 import AuctionFactory from "./AuctionFactory.json";
 
-const auctionFactoryAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // Replace with your contract address
+const auctionFactoryAddress = "0x8604b9FF3F3A6F68F34A6588F0D48dE74d634E42"; // Replace with your contract address
 
 // Ensure the provider is available before continuing
 let auctionFactoryContract: Contract;
